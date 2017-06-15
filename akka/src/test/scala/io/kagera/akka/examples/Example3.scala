6ec70ca386fb8f3c3052b69893ca20eb87cd3def
Md. Adil Akhter <md.adilakhter@gmail.com>
2017-06-15 20:40:56 +0200

    more examples added

--- akka/src/test/scala/io/kagera/akka/examples/Example3.scala
+++ akka/src/test/scala/io/kagera/akka/examples/Example3.scala
@@ -1,30 +1,17 @@
 package io.kagera.akka.examples
 
-import akka.actor.{ ActorSystem, Props }
-import akka.util.Timeout
-import io.kagera.akka.AkkaTestBase
-import io.kagera.akka.actor.PetriNetInstance
-import io.kagera.akka.actor.PetriNetInstanceProtocol.{ FireTransition, Initialize, Initialized, TransitionFired }
-import io.kagera.api.{ Marking, PetriNet }
-import io.kagera.dsl.experiment.{ Place, RuntimeTransition, Transition, buildPetriNet, petriNetRuntime, placeId, transitionId, |> }
-
-import scala.concurrent.ExecutionContext
 import akka.actor.Props
 import akka.util.Timeout
 import io.kagera.akka.AkkaTestBase
 import io.kagera.akka.actor.PetriNetInstance
-import io.kagera.akka.actor.PetriNetInstanceProtocol._
-import io.kagera.akka.examples.Example2.analysis
-import io.kagera.api._
-import io.kagera.dsl.experiment._
+import io.kagera.akka.actor.PetriNetInstanceProtocol.{ FireTransition, Initialize, Initialized }
+import io.kagera.api.{ Marking, PetriNet, _ }
+import io.kagera.dsl.experiment.{ Place, RuntimeTransition, Transition, buildPetriNet, petriNetRuntime, placeId, transitionId, |>, _ }
 import org.scalatest.Matchers._
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.mock.MockitoSugar
 
-import scala.concurrent.{ Await, ExecutionContext }
-import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
 
-object Example2 extends App {
+object Example3 extends App {
   object utils extends AkkaTestBase {
     def petriNetProps(topology: PetriNet[Place[_], RuntimeTransition[_, _]]) =
       Props(new PetriNetInstance[Place, RuntimeTransition, Unit, Unit](
@@ -58,28 +45,20 @@ object Example2 extends App {
   val pTomatoSauce = Place[TomatoSauce](id = "pTomatoSauce")
   val pDone = Place[Done](id = "pDone")
 
-  val doGroceries = Transition((x: StartCooking) ⇒ (Tomatoes(""), Pasta("")))
+  val doGroceries = Transition(() ⇒ (Tomatoes(""), Pasta("")))
   val boilThePasta = Transition((x: Pasta) ⇒ BoiledPasta("")-<>)
   val cookTomatoSauce = Transition((x: Tomatoes) ⇒ TomatoSauce("")-<>)
   val serveDinner = Transition((x: BoiledPasta, y: TomatoSauce) ⇒ { println("processing done: "); Done()-<> })
 
   val net =
     buildPetriNet(
-      |>(pInit) ~> (doGroceries) ~>> (pTomatoes, pPasta),
+      |>(doGroceries) ~>> (pTomatoes, pPasta),
       |>(pPasta) ~> boilThePasta ~>> |>(pBoiledPasta),
       |>(pTomatoes) ~> cookTomatoSauce ~>> |>(pTomatoSauce),
       (pBoiledPasta, pTomatoSauce) ~> serveDinner ~>> |>(pDone)
     )
 
-  val initialMarking = Marking((pInit(StartCooking())))
-  val initialMarking2 = Marking.empty[Place]
-
-  val analysis = new PetriNetAnalysis(net)
-  analysis.isReachable(initialMarking.multiplicities, Map(pDone -> 1)) shouldBe (true)
-
-  val res = analysis.isReachable(initialMarking2.multiplicities, Map(pDone -> 1))
-
-  println("isreachable: " + res)
+  val initialMarking = Marking.empty[Place]
 
   val instance = system.actorOf(petriNetProps(net))
 
